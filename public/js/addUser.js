f7d084494c1a508cc436b0aec6fda2c5e29b6182
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2023-10-22 01:29:56 +0800

    fix form fields + some backend connection template

--- public/js/addUser.js
+++ public/js/addUser.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
 
-    $("#userTypeSelect").change(function() {
-        if ($(this).val() == "SEDO") {
-            $("#addUser").load("addUser", { userType: "SEDO" });
-        } else if ($(this).val() == "Treasurer") {
-            $("#addUser").load("addUser", { userType: "Treasurer" });
-        }
-    });
+    
 
+    // check #clusterSelect element type
+    if($("#clusterSelect").is('select')){
+    
+        // Changes the available options for the self help group select
+        $("#clusterSelect").change(function() {
+            // I do not know how slow this can be
+            $('#groupSelect option:not(:first)').remove();
+            let newOption = `<option value="test">NEW OPTION</option>`
+            $("#groupSelect").append(newOption);
+            $("#groupSelect").append(newOption);
+            $("#groupSelect").append(newOption);
+        });
+    }
+    // if the user is a SEDO
+    else if ($("#clusterSelect").is('input')){
+        //append options to the select here OR do it in the ejs file
+        //for ejs file, make sure to put an if auth === SEDO before adding options
+    }
 });
