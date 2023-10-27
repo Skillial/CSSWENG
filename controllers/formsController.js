7641131bcd84772d743f9847031200df8467c128
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2023-10-28 00:54:55 +0800

    all forms added

--- controllers/formsController.js
+++ controllers/formsController.js
@@ -1,6 +1,7 @@
 const Cluster = require('../models/Cluster');
 const Project = require('../models/Project');
 const User = require('../models/User');
+const SHG = require('../models/Group');
 
 const mongoose = require('mongoose')
 
@@ -16,7 +17,12 @@ const formsController = {
         const projectName = req.params.projectName;
         const project = await Project.findOne({name: projectName});
         res.render('components/popupFields/Sub-ProjectsFormFields', {project});
-    }
+    },
+    loadEditSHGForm: async (req, res) => {
+        const shgName = req.params.shgName;
+        const shg = await SHG.findOne({name: shgName});
+        res.render('components/popupFields/SHGFormFields', {shg});
+    },
 }
 
 module.exports = formsController;
--- routers/routesForms.js
+++ routers/routesForms.js
@@ -9,31 +9,8 @@ const Cluster = require('../models/Cluster');
 
 const formsController = require('../controllers/formsController');
 
-//This is one just to render the forms in a separate page
-router.get("/addUser", async (req, res) => {
-    if(req.session.isLoggedIn){
-        const user = await User.findById(req.session.userId);
-        const authority = user.authority;
-        var clusters=[]; // sample
-        if (authority == "Admin"){
-            clusters = await Cluster.find({});
-        }
-        if (authority == "SEDO"){
-            clusters = await Cluster.find({validSEDOs : [user]});
-        }
-        res.render("popup", { authority, clusters });
-    }
-    else
-        res.redirect("/");
-});
-
-//This one changes the form depending on the user type selected
-router.post("/addUser", async (req, res) => {
-    res.render("components/addUser", { userType: req.body.userType });
-});
-
-
 //ACTUAL ROUTES TO BE USED
 router.get('/editClusterForm/:clusterName', formsController.loadEditClusterForm);
 router.get('/editSubProjectsForm/:projectName', formsController.loadEditSubProjectsForm);
+router.get('/editSHGForm/:shgName', formsController.loadEditSHGForm);
 module.exports = router;
